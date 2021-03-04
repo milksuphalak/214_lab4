a191325408a627df713e0c13f60eada58cc36b9d
milksuphalak <suphalak.l@ku.th>
2021-03-04 15:33:10 +0700

    3

--- 4.3.c
+++ 4.3.c
@@ -2,17 +2,19 @@
 #include <string.h>
 int main ()
 {
-    int i,j=0;
+    int i,j=1;
     char m[200],n[200];
     scanf ("%s",m);
+    n[0]=m[0];
     for (i=0;i<strlen(m);i++)
     {
+        if (m[i]=='\0')
+            break;
         if (m[i]!=m[i+1])
         {
-            n[j]=m[i];
+            n[j]=m[i+1];
             j++;
         }
-
     }
     printf ("%s",n);
 return 0;
