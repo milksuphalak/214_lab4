16f63b4d32878b42455e2cf8a474dba7b91a63ca
milksuphalak <suphalak.l@ku.th>
2021-03-04 17:47:34 +0700

    1

--- 4.5.c
+++ 4.5.c
@@ -3,13 +3,13 @@ int main()
 {
     int n,a[4]= {1,0,0,0},m;
     scanf("%d",&n);
-    char s[n][50];
+    char x[n][50];
     for(int i=0; i<n; i++)
     {
-        scanf("%s",s[i]);
-        for(int j=0; s[i][j]!='\0'; j++)
+        scanf("%s",x[i]);
+        for(int j=0; x[i][j]!='\0'; j++)
         {
-            switch (s[i][j])
+            switch (x[i][j])
             {
             case 'A':
                 m=a[1];
@@ -47,7 +47,16 @@ int main()
                 break;
             }
         }
-
+        for(int j=0; j<4; j++)
+        {
+            if(a[j]==1)
+            {
+                printf("%d\n",j+1);
+                a[j]=0;
+                break;
+            }
+        }
+        a[0]=1;
     }
     return 0;
 }
